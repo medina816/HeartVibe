1a6e72b0e88b6c158966f20ce7877ec40d13a8db
medina816 <your_email@example.com>
2025-03-29 20:09:11 +0600

    added structure

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Header/Header'
+import { Outlet } from 'react-router-dom'
+import Footer from '../Footer/Footer'
+function Layout() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Layout
