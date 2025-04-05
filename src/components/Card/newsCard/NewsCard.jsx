ab25d09a9a22dd01a38cd46bf859605367b30725
nurhan004 <sharshenbekov777@gmail.com>
2025-04-06 00:44:32 +0600

    обновленный адаптация news 2

--- src/components/Card/newsCard/NewsCard.jsx
+++ src/components/Card/newsCard/NewsCard.jsx
@@ -7,16 +7,22 @@ function NewsCard({ data }) {
   return (
     <div className="news-card">
       <div className="news-card-image">
-        <img src={data.image}  alt={data.title || "Новость"} 
-          loading="lazy"/>
+        <img 
+          src={data.image} 
+          alt={data.title || "Новость"} 
+          loading="lazy"
+        />
       </div>
+      
       <div className="news-card-content">
         <div className="news-date">
           <img src={calendar} alt="Календарь" />
           <span>{data.created_at}</span>
         </div>
+        
         <h3 className="news-title">{data.title}</h3>
         <p className="news-text">{data.content}</p>
+        
         <Link to={`/news/${data.id}`} className="news-link">
           Читать подробнее
         </Link>
